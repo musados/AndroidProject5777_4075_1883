98fb5630f81bd555d806603ec7b9682b1f3c7ff8
musados <musados@gmail.com>
2017-01-25 01:47:11 +0200

    ListDB preparing (Add, Remove and Update)

---
+++ app/src/main/java/com/siduron/java/iTravel/model/datasource/EntitiesUriCodes.java
@@ -0,0 +1,12 @@
+package com.siduron.java.iTravel.model.datasource;
+
+/**
+ * Created by musad on 24/01/2017.
+ */
+
+/**
+ * Codes of uri for the uri matchers
+ */
+public enum EntitiesUriCodes {
+    USER_CODE,BUSSINESS_CODE,ACTIVITY_CODE,ACTIVITY_ADAPTER_CODE;
+}
---
+++ app/src/main/java/com/siduron/java/iTravel/model/entities/DBType.java
@@ -0,0 +1,9 @@
+package com.siduron.java.iTravel.model.entities;
+
+/**
+ * Created by musad on 24/01/2017.
+ */
+
+public enum DBType {
+    LIST,WEB_DB;
+}
